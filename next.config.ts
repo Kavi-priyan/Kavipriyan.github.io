1db9b8ddc85a0ea0190ef9cb078421cb53af8832
Kavipriyan <131338635+Kavi-priyan@users.noreply.github.com>
2025-06-22 12:45:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig = {
-  output: 'export',
-  basePath: isGithubPages ? '/Kavipriyan.github.io' : '',
-  assetPrefix: isGithubPages ? '/Kavipriyan.github.io/' : '',
-  images: {
-    unoptimized: true,
-  },
+  output: 'export', // 👈 tells Next.js to statically export
+  // basePath: '/<repo-name>', 👈 optional if you're not deploying to a subfolder
 };
 
 module.exports = nextConfig;
